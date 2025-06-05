771b0259a92039854ce8dff050342431109cf57b
leraroy <valeria98.val@gmail.com>
2025-06-05 16:47:26 +0300

    init

--- helper/generate_config.py
+++ helper/generate_config.py
@@ -2,17 +2,22 @@ import os
 from dotenv import load_dotenv
 from string import Template
 from pathlib import Path
+from config import *
 
 def absolute_path(filename):
     project_root = Path(__file__).resolve().parent.parent
     path_body = project_root / '.circleci' / filename
     return path_body
 
+env = dict(os.environ)
+env.setdefault("QA_API_URL", API_URL)
+env.setdefault("QA_TOKEN", TOKEN)
+env.setdefault("QA_TEAM_ID", TEAM_ID)
 
 with open(f"{absolute_path('config_template.yml')}") as f:
     template = Template(f.read())
+    rendered = template.substitute(env)
 
-rendered = template.substitute(os.environ)
 
 with open(f"{absolute_path('config.yml')}", "w") as f:
     f.write(rendered)
