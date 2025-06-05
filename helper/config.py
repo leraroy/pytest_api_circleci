e9aa70d3baf5e790031eedfe3758d1d0bc686569
leraroy <valeria98.val@gmail.com>
2025-06-05 16:17:15 +0300

    init

--- helper/config.py
+++ helper/config.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-from string import Template
 
 load_dotenv()
 env = os.getenv("ENVIRONMENT")
@@ -9,13 +8,3 @@ API_URL = os.getenv(f"{env}_API_URL")
 TOKEN = os.getenv(f"{env}_TOKEN")
 TEAM_ID = os.getenv(f"{env}_TEAM_ID")
 FOLDER_ID = os.getenv(f"{env}_FOLDER_ID")
-
-with open("config_template.yml") as f:
-    template = Template(f.read())
-
-rendered = template.substitute(os.environ)
-
-with open("config.yml", "w") as f:
-    f.write(rendered)
-
-print("✅ config.yml generated successfully.")
---
+++ helper/generate_config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from string import Template
+from pathlib import Path
+
+def absolute_path(filename):
+    project_root = Path(__file__).resolve().parent.parent
+    path_body = project_root / '.circleci' / filename
+    return path_body
+
+with open(f"{absolute_path('config_template.yml')}") as f:
+    template = Template(f.read())
+
+rendered = template.substitute(os.environ)
+
+with open(f"{absolute_path('config.yml')}", "w") as f:
+    f.write(rendered)
