b91b09e4b9246e02d0b6fc64db7caec73161a88a
leraroy <valeria98.val@gmail.com>
2025-06-05 15:56:32 +0300

    init

--- helper/config.py
+++ helper/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from string import Template
 
 load_dotenv()
 env = os.getenv("ENVIRONMENT")
@@ -9,4 +10,12 @@ TOKEN = os.getenv(f"{env}_TOKEN")
 TEAM_ID = os.getenv(f"{env}_TEAM_ID")
 FOLDER_ID = os.getenv(f"{env}_FOLDER_ID")
 
+with open("config_template.yml") as f:
+    template = Template(f.read())
 
+rendered = template.substitute(os.environ)
+
+with open("config.yml", "w") as f:
+    f.write(rendered)
+
+print("✅ config.yml generated successfully.")
--- tests/test_get_goals.py
+++ tests/test_get_goals.py
@@ -1,31 +1,31 @@
-import pytest
-
-from modules.DeleteGoal import DeleteGoal
-from modules.GetGoals import GetGoals
-from modules.CreateGoal import CreateGoal
-
-class TestGetGoals:
-
-    @pytest.fixture(autouse=True)
-    def setup_and_teardown(self, create_goal_with_delete):
-        self.get_goals = GetGoals()
-        self.create_goal = CreateGoal()
-        self.delete_goal = DeleteGoal()
-
-        self.goal_name = create_goal_with_delete.get('name')
-        self.goal_id = create_goal_with_delete.get('id')
-
-
-    def test_get_goals_return_200(self):
-        response = self.get_goals.get_goals()
-        assert response.status_code == 200
-
-    def test_contain_correct_name_and_id(self):
-        response = self.get_goals.get_goals()
-        assert self.goal_name in response.text
-
-    def test_get_goals_without_auth(self):
-        assert self.get_goals.get_goals_without_auth().status_code == 400
-
-    def test_get_goals_validate_schema(self):
-        assert self.get_goals.get_goals_schema_validate()
+# import pytest
+#
+# from modules.DeleteGoal import DeleteGoal
+# from modules.GetGoals import GetGoals
+# from modules.CreateGoal import CreateGoal
+#
+# class TestGetGoals:
+#
+#     @pytest.fixture(autouse=True)
+#     def setup_and_teardown(self, create_goal_with_delete):
+#         self.get_goals = GetGoals()
+#         self.create_goal = CreateGoal()
+#         self.delete_goal = DeleteGoal()
+#
+#         self.goal_name = create_goal_with_delete.get('name')
+#         self.goal_id = create_goal_with_delete.get('id')
+#
+#
+#     def test_get_goals_return_200(self):
+#         response = self.get_goals.get_goals()
+#         assert response.status_code == 200
+#
+#     def test_contain_correct_name_and_id(self):
+#         response = self.get_goals.get_goals()
+#         assert self.goal_name in response.text
+#
+#     def test_get_goals_without_auth(self):
+#         assert self.get_goals.get_goals_without_auth().status_code == 400
+#
+#     def test_get_goals_validate_schema(self):
+#         assert self.get_goals.get_goals_schema_validate()
